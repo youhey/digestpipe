91ad9f3a8c999e6d4c4e1f91d6e0ad82b98f542e
IKEDA Youhei <youhey.ikeda@gmail.com>
2026-06-04 00:40:22 +0900

    fix: use UTC cron schedule for run cycle

--- src/routes/console.php
+++ src/routes/console.php
@@ -2,8 +2,18 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('digestpipe:run-cycle --feed-limit=20 --item-limit=50 --max-seconds=600')
-    ->everyThirtyMinutes()
-    ->timezone('Asia/Tokyo')
-    ->between('08:00', '17:59')
+$command = 'digestpipe:run-cycle --feed-limit=20 --item-limit=50 --max-seconds=600';
+
+// JST 08:00 / 08:30 = UTC 23:00 / 23:30
+Schedule::command($command)
+    ->cron('0,30 23 * * *')
+    ->timezone('UTC')
+    ->name('digestpipe:run-cycle:jst-08')
+    ->withoutOverlapping(30);
+
+// JST 09:00 - 17:30 = UTC 00:00 - 08:30
+Schedule::command($command)
+    ->cron('0,30 0-8 * * *')
+    ->timezone('UTC')
+    ->name('digestpipe:run-cycle:jst-09-17')
     ->withoutOverlapping(30);
